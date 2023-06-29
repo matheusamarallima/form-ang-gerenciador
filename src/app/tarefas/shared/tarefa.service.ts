58650c2b59868acf463eeb92f35fa9757ce4b3ef
Mathe <matheusamrl@gmail.com>
2023-06-29 11:41:30 -0300

    Test commit

---
+++ src/app/tarefas/shared/tarefa.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Tarefa } from './tarefa.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TarefaService {
+
+  constructor() { }
+
+  listarTodos(): Tarefa[] {
+    const tarefas = localStorage['tarefas'];
+    return tarefas ? JSON.parse(tarefas): []; // ternário 
+  }
+}
